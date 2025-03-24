8c12b079b29972d533a4b319bfc68ce7b3f1171c
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-24 18:34:14 +0500

    added_mass_energy_calculator_using_einstein_formula

---
+++ Assignment-00-to-05/01_expressions/02_e=mc2.py
@@ -0,0 +1,22 @@
+C: int = 299792458  # The speed of light in m/s
+
+def main():
+    mass_in_kg: float = float(input("Enter kilos of mass: "))
+
+    # Calculate energy
+
+    energy_in_joules: float = mass_in_kg * (C ** 2)
+
+    # Display 
+
+    print("e = m * C^2...")
+    print("m = " + str(mass_in_kg) + " kg")
+    print("C = " + str(C) + " m/s")
+    
+    print(str(energy_in_joules) + " joules of energy!")
+
+
+
+
+
+main()
