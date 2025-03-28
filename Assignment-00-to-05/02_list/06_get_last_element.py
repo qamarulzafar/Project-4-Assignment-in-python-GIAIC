b6a41b42a589886380efabd6d3dda186bdf7fd56
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-28 21:32:23 +0500

    implemented_get_last_element_function

---
+++ Assignment-00-to-05/02_list/06_get_last_element.py
@@ -0,0 +1,11 @@
+# Fill out the function get_last_element(lst) which takes in a list lst as a parameter and prints the last element in the list. The list is guaranteed to be non-empty, but there are no guarantees on its length.
+
+
+
+def get__last_element(lst):
+    print(lst[-1])
+
+
+
+get__last_element([1,2,3,4,5,6,7])
+get__last_element(["apple", "banna", "mango"])
