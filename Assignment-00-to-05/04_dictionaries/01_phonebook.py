1692d6e55b85231632ab61cd2eb543d3c699b38f
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-04-01 02:54:05 +0500

    add_problem_statement

--- Assignment-00-to-05/04_dictionaries/01_phonebook.py
+++ Assignment-00-to-05/04_dictionaries/01_phonebook.py
@@ -1,8 +1,7 @@
+# In this program we show an example of using dictionaries to keep track of information in a phonebook.
+
 def read_phone_numbers():
-    """
-    Ask the user for names/numbers to story in a phonebook (dictionary).
-    Returns the phonebook.
-    """
+   
     phonebook = {}                   # Create empty phonebook
 
     while True:
@@ -14,20 +13,13 @@ def read_phone_numbers():
 
     return phonebook
 
-
 def print_phonebook(phonebook):
-    """
-    Prints out all the names/numbers in the phonebook.
-    """
+    
     for name in phonebook:
         print(str(name) + " -> " + str(phonebook[name]))
 
-
 def lookup_numbers(phonebook):
-    """
-    Allow the user to lookup phone numbers in the phonebook
-    by looking up the number associated with a name.
-    """
+   
     while True:
         name = input("Enter name to lookup: ")
         if name == "":
@@ -37,13 +29,9 @@ def lookup_numbers(phonebook):
         else:
             print(phonebook[name])
 
-
 def main():
     phonebook = read_phone_numbers()
     print_phonebook(phonebook)
     lookup_numbers(phonebook)
 
-
-# Python boilerplate.
-if __name__ == '__main__':
-    main()
+main()
