0b108fc5c5b9a28dd13c14449e0c03b75d1cce9e
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-04-03 15:40:22 +0500

    added_fruit_stock_checker

---
+++ Assignment-00-to-05/07_information_flow/03_in_stock.py
@@ -0,0 +1,48 @@
+# Sophia has a fruit store. She has written a function num_in_stock which takes a string fruit as a parameter and returns how many of that fruit are in her inventory. Write code in main() which will:
+
+# Prompt the user to enter a fruit ("Enter a fruit: ")
+
+# Call num_in_stock(fruit) to get the number of that fruit that Sophia has in stock
+
+# Print the number which are in stock if Sophia has that fruit in her inventory (there are more than 0 in stock)
+
+# Print "This fruit is not in stock." if Sophia has none of that fruit in her inventory.
+
+# Here's two sample runs of the program (user input in bold italics):
+
+# Enter a fruit: pear
+
+# This fruit is in stock! Here is how many:
+
+# 1000
+
+# Enter a fruit: lychee
+
+# This fruit is not in stock.
+
+
+inventry = {
+    "apple": 500,
+    "banana": 300,
+    "pear": 1000,
+    "orange": 150,
+    "grape": 50
+}
+
+
+def num_in_stock(fruit: str) :
+    return inventry.get(fruit.lower(), 0)
+
+def main():
+    fruit = input("Enter a fruit: ").strip().lower()
+    stock = num_in_stock(fruit)
+
+    
+    if stock > 0:
+        print(f"This fruit is in stock! Here is how many:")
+        print(stock)
+    else:
+        print(f"This fruit is not in stock.")
+
+if __name__ == "__main__":
+    main()
