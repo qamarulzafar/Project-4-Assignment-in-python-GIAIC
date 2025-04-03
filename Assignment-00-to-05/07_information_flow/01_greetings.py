e2432652799dea14166cb7571f222ca5726f22f0
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-04-03 14:57:48 +0500

    added_greet_function_to_welcome_users

---
+++ Assignment-00-to-05/07_information_flow/01_greetings.py
@@ -0,0 +1,19 @@
+# We've written a helper function for you called greet(name) which takes as input a string name and prints a greeting. Write some code in main() to get the user's name and then greet them, being sure to call the greet(name) helper function.
+
+# Here's a sample run of the program (user input in bold italics):
+
+# What's your name? Sophia
+
+# Greetings Sophia!
+
+
+def greet(name):
+    return "Greeting" + name + "!"
+
+def main():
+    name:str = input("What's your name? ")
+    print(greet(name))
+
+
+
+main()
